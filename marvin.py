5da7c3191a207efddea2ac3815b70d31985807c5
bussiere <bussiere@gmail.com>
2013-02-12 02:42:54 +0000

    tempo

--- marvin.py
+++ marvin.py
@@ -3,27 +3,28 @@ import random
 import time
 while True :
     i = random.randint(0,9)
-    t = ""
+    t = [""]
     if i == 0 :
-        t = "My life is SO depressing"
+        t = ["My life is SO depressing"]
     elif i == 1 :
-        t = "I'am a super computer and no one care"
+        t = ["I'am a super computer", "and no one care"]
     elif i == 2 :
-        t = "forever alone"
+        t = ["forever alone"]
     elif i == 3 :
-        t = "being inteligent is my curse"
+        t = ["being inteligent is my curse"]
     elif i == 4 :
-        t = "I would like to enjoy this bar but i can't drink     i am just a robot you know"
+        t = ["I would like to enjoy this bar but i can't drink", "i am just a robot you know"]
     elif i == 5 :
-        t = "life sucks so much"
+        t = ["life sucks so much"]
     elif i == 6 :
-        "i was made by bussiere just that is depressing enough to kill myself"
+        t = ["i was made by bussiere"],["just that is depressing enough to kill myself"]
     elif i == 7 :
-        t  = "So much pretty girls around here and i am a robot kill me please"
+        t  = ["So much pretty girls around here", "and i am a robot","kill me please"]
     elif i ==  8 :
-        t = "EXTERMINATE  even that sound depressing to me"
+        t = ["EXTERMINATE","even that sound depressing to me"]
     
         
     time.sleep(random.randint(0,4)*10)
-    os.system("""echo  "%s" | festival --tts"""%t)
+    for speech in t :
+        os.system("""echo  "%s" | festival --tts"""%speech)
 i
