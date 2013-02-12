29c4d55f3d406a7ef757d05f5c360e0b8f264b9b
bussiere <bussiere@gmail.com>
2013-02-12 01:20:58 +0000

    marvin le depressif

---
+++ marvin.py
@@ -0,0 +1,26 @@
+import os
+import random
+import time
+while True :
+    i = random.randint(0,8)
+    t = ""
+    if i == 0 :
+        t = "My life is SO depressing"
+    elif i == 1 :
+        t = "I'am a super computer and no one care"
+    elif i == 2 :
+        t = "forever alone"
+    elif i == 3 :
+        t = "being inteligent is my curse"
+    elif i == 4 :
+        t = "I would like to enjoy this bar but i can't drink     i am just a robot you know"
+    elif i == 5 :
+        t = "life sucks so much"
+    elif i == 6 :
+        "i was made by bussiere just that is depressing enough to kill myself"
+    elif i == 7 :
+        "So much pretty girls around here and i am a robot kill me please"
+    
+        
+    time.sleep(random.randint(0,4)*10)
+    os.system("""echo  "%s" | festival --tts"""%t)
